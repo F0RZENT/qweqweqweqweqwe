b824d385de8a3ba475c4bba466b028acf95a4c84
F0RZENT <113331099+F0RZENT@users.noreply.github.com>
2023-11-17 12:27:31 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,32 @@
 ﻿using System.Net.Sockets;
-using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 
-using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
 try{
-    await tcpClient.ConnectAsync("192.168.220.139", 1776);
+    await tcpClient.ConnectAsync("192.168.220.139", 1777);
     while(true)
     {
 
-        string path = ".img/";
+        string path = ".img/qwe.jpg";
 
-        byte[] reqestData = Encoding.UTF8.GetBytes(path);
+        byte[] reqestData = File.ReadAllBytes(path);
 
         await tcpClient.SendAsync(reqestData);
 
-        Console.WriteLine("Сообщение отправленно!");
-
-        byte[] data = new byte[512];
-
-        int bytes = await tcpClient.ReceiveAsync(data);
-
-        string time = Encoding.UTF8.GetString(data, 0, bytes);
-
-        Console.WriteLine("");
-        
-        
+        Console.WriteLine("Изображение отправленно!");
         
     }
 }
 catch (Exception ex){
     Console.WriteLine(ex.Message);
 }
+
+
+       // byte[] data = new byte[512];
+
+        // int bytes = await tcpClient.ReceiveAsync(data);
+
+        // string time = Encoding.UTF8.GetString(data, 0, bytes);
+
+        // Console.WriteLine("");
+
